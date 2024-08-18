358034377a417e716b2d78f19af3baa6ba2f70f6
Basu Slathia <basuslathia01@gmail.com>
2024-08-19 00:08:58 +0530

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,45 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"enter a no.";
+//     cin>>a;
+//     cout<<a<<"leatn codng";
+//     return 0;
+
+// }
+
+
+// // // You are using GCC
+// // #include<iostream>
+// // using namespace std;
+
+// // int main(){
+    
+// //     int a;
+// //     cout<<"ENTER number"<<endl;
+// //     cin>>a;
+    
+// //     if(a>0){
+// //         cout<<"your number is positive"<<endl;
+// //         cout<<a<<endl;
+// //     }
+// //     else{
+// //         cout<<"your number is negative"<<endl;
+// //         cout<<a<<endl;
+// //     }
+    
+// // }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a,b;
+    
+//     cin>>a >>b ;
+    
+//     cout<<"value of a and b is "<<a << " " <<b <<endl;
+    
+// }
