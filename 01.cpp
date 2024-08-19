f555bc11045ddf4995a9be8fb013afede433ba0f
Basu Slathia <basuslathia01@gmail.com>
2024-08-19 17:20:07 +0530

    Update 01.cpp

--- 01.cpp
+++ 01.cpp
@@ -45,3 +45,70 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=1;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<" " <<count ;
+//     count++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=1;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i;
+//             cout<<endl;
+//             count++;
+//         }
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+    
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<(i-j+1);
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     char a='A';
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<a;
+//         }
+//         a++;
+//         cout<<endl;
+//     }
+// }
