793acc581f1be1636330b7b5ccb95d2cfc71a788
Basu Slathia <basuslathia01@gmail.com>
2024-08-19 19:14:32 +0530

    Update 01.cpp

--- 01.cpp
+++ 01.cpp
@@ -112,3 +112,113 @@
 //         cout<<endl;
 //     }
 // }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=1;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<" " <<count ;
+//     count++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=1;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i;
+//             cout<<endl;
+//             count++;
+//         }
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+    
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<(i-j+1);
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     char a='A';
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<a;
+//         }
+//         a++;
+//         cout<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     int even=0;
+//     int odd=1;
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     for(int i=0;i<n;i++){
+//       if(arr[i]%2==0){
+//           even=even+arr[i];
+//       }
+//       else{
+//           odd=odd*arr[i];
+//       }
+//     }
+//     cout<<"the sum of even no. is "<<even<<endl;
+//     cout<<"the mul of odd no. is "<<odd<<endl;
+    
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=10;
+//     int a=0;
+//     int b=1;
+//     cout<<a <<" "<<b<<" ";
+//     for(int i=1;i<=n;i++){
+//         int next=a+b;
+//         cout<<next<<" ";
+        
+//         a=b;
+//         b=next;
+//     }
+// }
