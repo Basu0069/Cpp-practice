4b43958a6b478384684f5e535092d3783ef6792f
Basu Slathia <basuslathia01@gmail.com>
2024-08-21 16:02:55 +0530

    Update 02_class.cpp
    practice

--- 02_class.cpp
+++ 02_class.cpp
@@ -1,37 +1,62 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
 
-class Employee
-{
-    private:
-     int a,b,c;
-    public:
-    int d, e;
-    void setData(int a1,int b1,int c1);
-    void getData(){
-        cout<<"The value of a is "<<a<<endl;
-        cout<<"The value of b is "<<b<<endl;
-        cout<<"The value of c is "<<c<<endl;
-        cout<<"The value of d is "<<d<<endl;
-        cout<<"The value of e is "<<e<<endl;
-    }
+// class Employee
+// {
+//     private:
+//      int a,b,c;
+//     public:
+//     int d, e;
+//     void setData(int a1,int b1,int c1);
+//     void getData(){
+//         cout<<"The value of a is "<<a<<endl;
+//         cout<<"The value of b is "<<b<<endl;
+//         cout<<"The value of c is "<<c<<endl;
+//         cout<<"The value of d is "<<d<<endl;
+//         cout<<"The value of e is "<<e<<endl;
+//     }
 
-};
+// };
 
-void Employee :: setData(int a1,int b1,int c1){
-    a= a1;
-    b= b1;
-    c= c1;
-}
-int main(){
-    Employee harry;
-    harry.d=34;
-    harry.e=21;
-    harry.setData(1,2,4);
-    harry.getData();
-    return 0;
-}
+// void Employee :: setData(int a1,int b1,int c1){
+//     a= a1;
+//     b= b1;
+//     c= c1;
+// }
+// int main(){
+//     Employee harry;
+//     harry.d=34;
+//     harry.e=21;
+//     harry.setData(1,2,4);
+//     harry.getData();
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int num=2;
+//     cout<<endl;
+//     switch(num){
+//         case 1:cout<<"First"<<endl;
+//         break;
+
+//         case 2: cout<<"second"<<endl;
+//         break;
+
+//         default: cout<<"Ti "
+//     }
+// }
+
+
+
+
+
+
+
 
 
 
