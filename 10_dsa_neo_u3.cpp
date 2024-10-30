18800abb5b0c331a5c80d51d1451a162f918ddf8
Basu Slathia <basuslathia01@gmail.com>
2024-10-30 22:52:49 +0530

    Create 10_dsa_neo_u3.cpp

---
+++ 10_dsa_neo_u3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class SimpleQueue{
+    private:
+    vector<int> arr;
+
+public:
+void enqueue(int customerID){
+
+}
+
+
+
+}
+
+
+
+
+
