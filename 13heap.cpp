a3504ec0241e84df9bebef4f3a857bbe915ad180
Basu Slathia <basuslathia01@gmail.com>
2024-11-19 21:40:54 +0530

    Create 13heap.cpp

---
+++ 13heap.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+
+    vector<int>a;
+
+    for(int i=0;i<n;i++){
+        int element;
+        cin>>element;
+        a.push_back(element);
+    push_heap(a.begin(),a.end(),greater<int>());
+    }
+cout<<endl;
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+
+cout<<endl;
+    int f;
+    cin>>f;
+    for(int i=0;i<a.size();i++){
+        if(f==a[i]){
+            cout<<f<<"is present";
+
+            break;
+        }
+        else{
+            cout<<f<<"is not present";
+            break;
+        }
+    }
+
+}
